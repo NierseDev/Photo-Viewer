5ee3645c65e1ddb537d6dedf0e3c068fc007fc8a
Nier'seDev <nienachuraru@yahoo.com>
2024-12-21 08:48:04 +0800

    Added additional import

--- photo-viewer.py
+++ photo-viewer.py
@@ -4,6 +4,7 @@ import shutil
 import functools
 import numpy as np
 import tkinter as tk
+import customtkinter as ctk
 from tkinter import font
 from tkinter import filedialog
 from PIL import Image, ImageTk, ImageOps, ImageFont, ImageDraw
